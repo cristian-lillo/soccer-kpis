c6958ad754b4c6dee54c0049388c4a047b075908
cristian-lillo <cristian.lillo@ug.uchile.cl>
2025-11-06 11:19:37 -0300

    style(Rater): improve code formatting and documentation

--- models/playerank/playerank/models/Rater.py
+++ models/playerank/playerank/models/Rater.py
@@ -1,57 +1,65 @@
-# /usr/local/bin/python
-from collections import defaultdict, OrderedDict, Counter
 import numpy as np
-
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-class Rater():
-    """Performance rating
+
+class Rater:
+    """
+    Performance rating.
 
     Parameters
     ----------
-    alpha_goal: float
-        importance of the goal in the evaluation of performance, in the range [0, 1]
-        default=0.0
+    alpha_goal : float, default=0.0
+        Importance of the goal in the evaluation of performance, in the range [0, 1].
 
     Attributes
     ----------
-    ratings_: numpy array
-        the ratings of the performances
+    ratings_ : numpy array
+        Ratings of the performances.
     """
+
     def __init__(self, alpha_goal=0.0):
         self.alpha_goal = alpha_goal
         self.ratings_ = []
 
-    def get_rating(self, weighted_sum, goals):
+    def get_rating(self, weighted_sum: float, goals: float) -> float:
         return weighted_sum * (1 - self.alpha_goal) + self.alpha_goal * goals
 
-    def predict(self, dataframe, goal_feature, score_feature, filename='ratings'):
+    def predict(self, dataframe: pd.DataFrame, goal_feature: str, score_feature: str) -> np.ndarray:
         """
-        Compute the rating of each performance in X
+        Compute the rating of each performance in X.
 
         Parameters
         ----------
-        dataframe: dataframe of playerank scores
-        goal_feature: column name for goal scored dataframe column
-        score_feature: column name for playerank score dataframe column
-
+        dataframe : pd.DataFrame
+            DataFrame of PlayeRank scores.
+        goal_feature : str
+            Column name for goal scored DataFrame column.
+        score_feature : str
+            Column name for playerank score DataFrame column.
 
         Returns
         -------
-        ratings_: numpy array
+        ratings_ : numpy array
         """
+        # Extract feature names and values
         feature_names = dataframe.columns
         X = dataframe.values
 
-        for i, row in enumerate(X):
+        # Get indices for goal and score features
+        goal_index = feature_names.get_loc(goal_feature)
+        pr_index = feature_names.get_loc(score_feature)
 
-            goal_index = feature_names.get_loc(goal_feature)
-            pr_index = feature_names.get_loc(score_feature)
+        # Compute ratings for each performance
+        ratings = []
+        for row in X:
+            rating = self.get_rating(
+                float(row[pr_index]),
+                float(row[goal_index]),
+            )
+            ratings.append(rating)
 
-            rating = self.get_rating(float(row[pr_index]), float(row[goal_index]),)
-            self.ratings_.append(rating)
-        self.ratings_ = MinMaxScaler().fit_transform(np.array(self.ratings_).reshape(-1, 1))[:, 0]
-
-
+        # Scale ratings to [0, 1] range
+        self.ratings_ = MinMaxScaler().fit_transform(np.array(ratings).reshape(-1, 1))[:, 0]
 
         return self.ratings_
