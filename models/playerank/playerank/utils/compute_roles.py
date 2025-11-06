8344bf029333ee7f9f1af60fd830af221ee126a8
cristian-lillo <cristian.lillo@ug.uchile.cl>
2025-11-06 11:34:15 -0300

    style(compute_roles): improve code formatting and structure

--- models/playerank/playerank/utils/compute_roles.py
+++ models/playerank/playerank/utils/compute_roles.py
@@ -1,29 +1,43 @@
-#import .models
-from ..models import Clusterer
+import json
+import sys
+from pathlib import Path
 
 from ..features import centerOfPerformanceFeature, plainAggregation
-import sys,json
-#computing all quality features (passes accurate, passes failed, shots, etc.)
+from ..models import Clusterer
 
+# Add the project root to the Python path
+sys.path.append(str(Path(__file__).parents[4]))
+
+from config import project_paths
+
+# Define Wyscout data file paths
+EVENTS_PATHS = str(project_paths.WYSCOUT_PAPPALARDO_DIR / "events" / "*.json")
+PLAYERS_FILEPATH = str(project_paths.WYSCOUT_PAPPALARDO_DIR / "players.json")
+
+# Define obtained features file paths
+ROLE_MATRIX_FILEPATH = str(Path(__file__).parents[2] / "role_matrix.json")
+
+
+# Compute all quality features (passes accurate, passes failed, shots, etc.)
 def compute_roleMatrix(output_path):
-    #getting average position for each player in each match
+    # Get average position for each player in each match
     centerfeat = centerOfPerformanceFeature.centerOfPerformanceFeature()
-    centerfeat = centerfeat.createFeature(events_path = 'playerank/data/events',
-                        players_file='playerank/data/players.json')
+    centerfeat = centerfeat.createFeature(
+        events_path=EVENTS_PATHS,
+        players_file=PLAYERS_FILEPATH,
+    )
 
-    #plain aggregation to get a dataframe
+    # Do plain aggregation to get a dataframe
     aggregation = plainAggregation.plainAggregation()
     aggregation.set_features([centerfeat])
-    df = aggregation.aggregate(to_dataframe = True )
+    df = aggregation.aggregate(to_dataframe=True)
 
-    #use clustering object to get the best fit
+    # Use clustering object to get the best fit
     clusterer = Clusterer.Clusterer(verbose=True, k_range=(8, 9))
-    clusterer.fit(df.entity, df.match, df[['avg_x', 'avg_y']], kind='multi')
+    clusterer.fit(df.entity, df.match, df[["avg_x", "avg_y"]], kind="multi")
+    matrix_role = clusterer.get_clusters_matrix(kind="multi")
 
-    matrix_role = clusterer.get_clusters_matrix(kind = 'multi')
-
-    matrix_role = matrix_role
-    json.dump(matrix_role,open(output_path,'w'))
+    json.dump(matrix_role, open(output_path, "w"))
 
 
-compute_roleMatrix('playerank/conf/role_matrix.json')
+compute_roleMatrix(ROLE_MATRIX_FILEPATH)
